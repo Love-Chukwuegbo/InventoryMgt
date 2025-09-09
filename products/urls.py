5a1cc787aa9c6d428ce3b47798df5efe693f80b8
Love-Chukwuegbo <cchinweokenwa@gmail.com>
2025-09-09 16:46:52 +0100

    Feat:changed database, added new serializer,url routes and views for the inventory model

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet,ProductInventoryList
 
 
 router = DefaultRouter()
@@ -9,5 +9,6 @@ router.register(r"product", ProductViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("inventory", ProductInventoryList),
     
 ]
